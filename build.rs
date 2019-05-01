7ca4b12ef5d4153efc3dec88fb40ab6f905d3f74
Joe Frikker <jfrikker@gmail.com>
2019-04-30 20:07:46 -0400

    initial grpc

--- build.rs
+++ build.rs
@@ -1,4 +1,10 @@
 fn main() {
-    prost_build::compile_protos(&["src/arbiter.proto"],
-                                &["src/"]).unwrap();
+    tower_grpc_build::Config::new()
+        .enable_server(true)
+        .enable_client(false)
+        .build(
+            &["proto/arbiter.proto"],
+            &["proto"],
+        )
+        .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
 }
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,35 @@ mod arbiter_fut;
 mod messages;
 
 use log::{error, info};
+use messages::{StartTransactionRequest, StartTransactionResponse};
 use std::rc::Rc;
 use std::cell::RefCell;
-use tokio::prelude::*;
-use tokio::codec::Framed;
-use tokio::codec::length_delimited::Builder;
+use tokio::runtime::current_thread::{TaskExecutor, run, spawn};
 use tokio::net::TcpListener;
-use tokio::runtime::current_thread::{run, spawn};
-use tokio::sync::mpsc::unbounded_channel;
-/*
-type State = Rc<RefCell<coordinator::Coordinator>>;
+use tokio::prelude::*;
+use tower_grpc::{Request, Response, Status};
+use tower_h2::Server;
+
+type TID = u64;
+type RID = String;
+type State = arbiter_fut::ArbiterFut<TID, RID>;
+
+#[derive(Clone)]
+struct ArbiterHandler {
+    state: Rc<RefCell<State>>
+}
+
+impl messages::server::Arbiter for ArbiterHandler {
+    type StartTransactionFuture = future::FutureResult<Response<StartTransactionResponse>, Status>;
+    fn start_transaction(&mut self, request: Request<StartTransactionRequest>) -> Self::StartTransactionFuture {
+        let req = request.get_ref();
+        let tid = req.tid;
+        let mut state = self.state.borrow_mut();
+        state.start_transaction(tid);
+        let result = StartTransactionResponse {};
+        future::ok(Response::new(result))
+    }
+}
 
 fn main() {
     stderrlog::new()
@@ -20,38 +39,34 @@ fn main() {
     .timestamp(stderrlog::Timestamp::Millisecond)
     .init().unwrap();
 
-    let arbiter = arbiter::Arbiter::new();
-    let coordinator = coordinator::Coordinator::new(arbiter);
-    let state: State = Rc::new(RefCell::new(coordinator));
-    let codec = Builder::new()
-        .big_endian()
-        .length_field_length(4)
-        .new_codec();
+    let handler = ArbiterHandler {
+        state: Rc::new(RefCell::new(
+            arbiter_fut::ArbiterFut::new()
+        ))
+    };
+
+    let new_service = messages::server::ArbiterServer::new(handler);
+
+    let h2_settings = Default::default();
+    let mut h2 = Server::new(new_service, h2_settings, TaskExecutor::current());
 
     let addr = "127.0.0.1:4585".parse().unwrap();
     info!("Listening on {}", addr);
     let listener = TcpListener::bind(&addr).expect("unable to bind TCP listener");
 
-    let server = listener.incoming()
-        .map_err(|e| error!("Error accepting connections: {}", e))
-        .for_each(|sock| {
-            let (write, read) = Framed::new(sock, codec).split();
-            let (sender, receiver) = unbounded_channel();
+    let server = listener
+        .incoming()
+        .for_each(move |sock| {
+            if let Err(e) = sock.set_nodelay(true) {
+                return Err(e);
+            }
 
-            let out = write
-                .sink_map_err(|e| error!("{}", e))
-                .send_all(receiver.map_err(|e| error!("{}", e)))
-                .map(|_| ());
-            spawn(out);
+            let serve = h2.serve(sock);
+            spawn(serve.map_err(|e| error!("h2 error: {:?}", e)));
 
-            let sid = state.borrow_mut().start_session(sender);
-            read.for_each(|message| {
-                state.borrow_mut().handle_message(sid, message);
-                Ok(())
-            });
             Ok(())
-        });
+        })
+        .map_err(|e| error!("accept error: {}", e));
 
     run(server);
 }
-*/
